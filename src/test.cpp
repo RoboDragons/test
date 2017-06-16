749cc9ab216b133a221ae57ef9b2f1b7fb3a64a1
masa-ito4github <masa-ito@ist.aichi-pu.ac.jp>
2017-06-16 14:00:06 +0900

    added "Thanks" line

--- src/test.cpp
+++ src/test.cpp
@@ -11,5 +11,6 @@ using namespace std;
 
 int main() {
 	cout << "!!!Hello RoboDragons!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!Thanks!!!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
